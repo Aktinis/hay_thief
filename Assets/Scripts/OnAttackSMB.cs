b46cb7a78bff8421eb055c6fa5424504770f5799
Deividas Krunkauskas <deividas.krunkauskas@gmail.com>
2023-09-21 16:45:30 +0300

    Remove unused values

--- Assets/Scripts/OnAttackSMB.cs
+++ Assets/Scripts/OnAttackSMB.cs
@@ -3,29 +3,16 @@ using UnityEngine;
 
 public class OnAttackSMB : StateMachineBehaviour
 {
-    private const float SLOW_DOWN_TIME_VALUE = 0.3f;
-    private const float DEFAULT_TIME_SCALE_TIME_VALUE = 1f;
-
-    private readonly float[] ANIMATION_TIME_KEY_VALUES = {0.25f, 0.35f, 0.5f };
+    private readonly float ANIMATION_TIME_KEY_VALUE = 0.35f;
 
     public Action DamageEnemies { get; set; }
     public Action OnExit { get; set; }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (stateInfo.normalizedTime >= ANIMATION_TIME_KEY_VALUES[0])
-        {
-            //Time.timeScale = SLOW_DOWN_TIME_VALUE;
-        }
-
-        if(stateInfo.normalizedTime >= ANIMATION_TIME_KEY_VALUES[1])
+        if(stateInfo.normalizedTime >= ANIMATION_TIME_KEY_VALUE)
         {
             DamageEnemies?.Invoke();
-        }
-
-        if(stateInfo.normalizedTime >= ANIMATION_TIME_KEY_VALUES[2])
-        {
-           // Time.timeScale = DEFAULT_TIME_SCALE_TIME_VALUE;
         }
     }
 
